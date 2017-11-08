5ad36a81f32851a4f52f4ccdb83810fdf0fb3c2d
Raffaele Sena <raff367@gmail.com>
2017-11-07 16:13:54 -0800

    Initial implementation

---
+++ mgot.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func homepath(p string) string {
+	if strings.HasPrefix(p, "~/") {
+		// relative to home
+		p := p[2:]
+		u, _ := user.Current()
+		return filepath.Join(u.HomeDir, p)
+	}
+
+	return p
+}
+
+func main() {
+	dbpath := flag.String("db", "~/Library/Messages/chat.db", "path to user Messages database")
+	service := flag.String("service", "SMS", "message service to query")
+	pattern := flag.String("pattern", "Your verification code is %.", "message text pattern")
+	filter := flag.Bool("filter", true, "filter out pattern")
+
+	flag.Parse()
+
+	db, err := sql.Open("sqlite3", homepath(*dbpath))
+	if err != nil {
+		log.Fatal(*dbpath, ": ", err)
+	}
+
+	defer db.Close()
+
+	var text string
+
+	err = db.QueryRow("SELECT text FROM message WHERE service=? AND text LIKE ? ORDER BY date DESC",
+		*service, *pattern).Scan(&text)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *filter {
+		re := strings.Replace(*pattern, "%", "(.+)", -1)
+		match := regexp.MustCompile(re).FindStringSubmatch(text)
+		if len(match) > 1 {
+			text = strings.Join(match[1:], " ")
+		}
+	}
+
+	fmt.Println(text)
+}
